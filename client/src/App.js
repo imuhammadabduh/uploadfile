ade6ca5f734cda0561789bfa3d8f9bba9ebf9591
imuhammadabduh <imuhammadabduh@gmail.com>
2023-05-09 22:42:30 +0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FileUpload from './components/FileUpload';
+
+const App = () => (
+  <div className='container-fluid mt-4'>
+    <h4 className='display-4 text-center mb-4'>
+       Cloud Mabduh
+    </h4>
+    <FileUpload />
+  </div>
+);
+
+
+export default App;
